24096b72cefba26b29b6f99034f7f0febf793a8e
Colt <Cewlt@users.noreply.github.com>
2016-05-25 21:47:42 -0400

    Update NoFall.java

--- com/colt/NoFall/NoFall.java
+++ com/colt/NoFall/NoFall.java
@@ -1,35 +1,43 @@
-package com.colt.NoFall;
+package colt.nofalldamage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
-import org.bukkit.World;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoFall extends JavaPlugin implements Listener {
-	
-	List<String> worlds = getConfig().getStringList("worlds");
-	
+
+	List<String> worlds = new ArrayList<String>();
+
 	public void onEnable() {
-		saveDefaultConfig();
 		Bukkit.getPluginManager().registerEvents(this, this);
+		saveDefaultConfig();
+
+		for (String world : getConfig().getStringList("worlds")) {
+			if (Bukkit.getWorld(world) == null) {
+				getLogger().info("Invalid world in config: " + world);
+			} else {
+				worlds.add(world);
+			}
+		}
 	}
 
 	@EventHandler
-	public void onEntityDamage(EntityDamageEvent e) {
-		if (e.getEntity() instanceof Player) {
-			Player p = (Player)e.getEntity();
-			if ((e.getCause().equals(EntityDamageEvent.DamageCause.FALL)) && (p.hasPermission("nofall.use"))) {
-			    for(String w : worlds) {
-			    	World world = Bukkit.getWorld(w);
-					if(p.getWorld().equals(world)) {
-						e.setCancelled(true);
-					}
-			    }
+	public void entityDamageEvent(EntityDamageEvent event) {
+		if (event.getEntityType() == EntityType.PLAYER) {
+			Player player = (Player) event.getEntity();
+			if (event.getCause() == DamageCause.FALL
+					&& player.hasPermission("nofall.use")) {
+				if (worlds.contains(player.getWorld().getName())) {
+					event.setCancelled(true);
+				}
 			}
 		}
 	}
